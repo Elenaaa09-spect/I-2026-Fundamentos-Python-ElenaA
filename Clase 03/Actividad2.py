3fae1dbf0e6187759865ca5ff605c1757fed670b
Elenaaa09-spect <elena.astorga.cespedes@gmail.com>
2026-05-23 22:21:59 -0600

    Clase 03

--- Clase 03/Actividad2.py
+++ Clase 03/Actividad2.py
@@ -1,5 +1,23 @@
-print ("Bienvenido a la aplicación para calcular su IMC")
-input ("Ingrese su nombre: Elena")
-input ("Ingrese sus apellidos: Astorga Céspedes")
-input ("Ingrese su peso en kilos: 63")
-input ("Ingrese su altura en metros: 1.65")
+print ("Bienvenido a la aplicación de cálculo de IMC")
+nombre = input ("Ingrese su nombre:")
+apellidos = input ("Ingrese sus apellidos: ")
+edad = int (input ("Ingrese su edad: "))
+peso = float (input("Ingrese su peso en KG: "))
+altura = float(input("Ingrese su altura en metros: "))
+
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    clasificacion = "Bajo peso"
+elif imc < 25:
+    clsificacion = "Peso normal"
+elif imc < 30:
+    clasificacion = "Sobrepeso"
+else:
+    clasificacion = "Obesidad"
+
+print ("\n--- RESULTADOS ---")
+print("Nombre:", nombre, apellidos)
+print("edad:", edad)
+print("IMC:", round(imc, 2))
+print("Clasificación:", clasificacion)
