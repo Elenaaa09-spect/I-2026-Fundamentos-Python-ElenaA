24e1c519a38b61e1655fe75fec55990853e5f49a
Elenaaa09-spect <elena.astorga.cespedes@gmail.com>
2026-06-03 22:17:02 -0600

    control_inventario_mejorado.py

---
+++ Prueba 1/control_inventario_mejorado.py
@@ -0,0 +1,44 @@
+#Recibimiento
+print ("Control de inventario")
+print ("Sea bienvenido")
+
+#Solicitar cantidad de productos
+cantidad_productos = int(input("¿Cuántos productos desea registrar?: "))
+
+#Variable para acumular eñ valor total de inventario
+total_inventario = 0
+
+#Ciclo para registrar productos
+for i in range (cantidad_productos):
+    print("\nProducto", i + 1)
+    nombre = input ("Nombre del producto: ")
+
+#Validar precio
+while True:
+    precio = float(input("Precio del producto: "))
+    if precio > 0 :
+        break
+    else:
+            print ("Error: el precio debe de ser mayor que 0")
+
+#Validar cantidad
+while True:
+     cantidad = int(input("Cantidad disponible: "))
+     if cantidad > 0:
+          break
+     else: print ("Error: la cantidad debe de ser mayor que 0")
+
+#Calcular valor total del producto
+valor_total = precio * cantidad
+
+#Acumular al inventario
+total_inventario += valor_total
+
+#Mostrar resultados del producto
+print ("\nResultado")
+print ("Producto:", nombre)
+print ("Valor total:", valor_total)
+
+#Estadística final
+print ("\n==== RESUMEN DEL INVENTARIO ====")
+print ("Valor total general del inventario:", total_inventario)
